bee13fd058d12a31526118a60c1c8ea8c8fb531a
veemtee <55981900+veemtee@users.noreply.github.com>
2021-04-14 21:09:32 +0300

    14 4

---
+++ SpaceShooter/SpaceShootteri/Assets/alkuruutuMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alkuruutuMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.Translate(Vector3.forward * 1000f * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
